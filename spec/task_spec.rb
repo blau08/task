285013a79515cf1877cdf78258a0cb36a727b34e
Evan Clough & Brian Lau <student@epicodus.com>
2015-08-11 09:37:00 -0700

    Write third spec. It will test that method #save saves a Task instance in the Task array.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -6,13 +6,22 @@ describe(Task) do
     it('lets you give it a description') do
       test_task = Task.new('scrub the zebra')
       expect(test_task.description()).to(eq('scrub the zebra'))
-
-      describe('.all') do
-        it('is empty at first') do
-          expect(Task.all()).to(eq([]))
-        end
-      end
-
     end
+
+    describe('.all') do
+      it('is empty at first') do
+        expect(Task.all()).to(eq([]))
+      end
+    end
+
+    describe('#save') do
+      it('adds a task to the array of saved tasks') do
+        test_task = Task.new("wash the lion")
+        test_task.save()
+        expect(Task.all()).to(eq([test_task]))
+      end
+    end
+
+
   end
 end
