8f6a004b6b156d91d1e9c21a91601bffd2d743a7
Evan Clough & Brian Lau <student@epicodus.com>
2015-08-11 09:33:11 -0700

    Define class method .all. It will return empty array when called on class Task.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -7,7 +7,7 @@ describe(Task) do
       test_task = Task.new('scrub the zebra')
       expect(test_task.description()).to(eq('scrub the zebra'))
 
-      describe ('.all') do
+      describe('.all') do
         it('is empty at first') do
           expect(Task.all()).to(eq([]))
         end
