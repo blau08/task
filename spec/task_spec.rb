db2a3234484f3236c1f0692085fa805806e58d85
Evan Clough & Brian Lau <student@epicodus.com>
2015-08-11 09:29:25 -0700

    Write second spec. It will test that Task method contains no instances.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -6,7 +6,13 @@ describe(Task) do
     it('lets you give it a description') do
       test_task = Task.new('scrub the zebra')
       expect(test_task.description()).to(eq('scrub the zebra'))
-      
+
+      describe ('.all') do
+        it('is empty at first') do
+          expect(Task.all()).to(eq([]))
+        end
+      end
+
     end
   end
 end
